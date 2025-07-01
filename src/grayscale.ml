2516712bb09830eae720ae3953753a2403a3cb40
StephenTrainor <stephontrainor@gmail.com>
2025-07-01 20:06:48 +0000

    Completed grayscale exercise with expect test

--- src/grayscale.ml
+++ src/grayscale.ml
@@ -2,8 +2,17 @@ open Core
 
 (* You need to change the implementation of this function so that it does something
    to the image instead of just leaving it untouched. *)
-let transform image =
-  image
+let transform image = Image.map ~f:Pixel.average image
+
+let%expect_test "grayscale_transform" =
+  let image_expected =
+    Image.load_ppm ~filename:"../images/reference-beach_portrait_gray.ppm"
+  in
+  let image_made =
+    Image.load_ppm ~filename:"../images/beach_portrait.ppm" |> transform
+  in
+  Image.image_comparison_report image_expected image_made;
+  [%expect {| Identical images |}]
 ;;
 
 let command =
@@ -20,5 +29,6 @@ let command =
         let image = Image.load_ppm ~filename |> transform in
         Image.save_ppm
           image
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
 ;;
